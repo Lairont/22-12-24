1f77ee63cd6eb3a70be650512ba6b42a4564f0b1
Никита <153456515+Lairont@users.noreply.github.com>
2024-10-22 11:16:35 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание2
+            //Console.Write("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //double p = (a + b + c) / 2;
+            //if (a < b + c && b < a + c && c < a + b)
+            //{
+            //    Console.WriteLine($"Треугольник существует");
+            //    Console.WriteLine($"S={Math.Sqrt(p * (p - a) * (p - b) * (p - c)):F2}");
+            //}
+            //else
+            //    Console.WriteLine($"Треугольник не существует");
+            //Console.ReadLine();
+
+                //задание1
+            //Console.Write("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //double d = Math.Pow(b, 2) - 4 * a * c;
+            //if (d > 0)
+            //{
+            //    double x1 = -b + Math.Sqrt(d) / 2 * a;
+            //    double x2 = -b - Math.Sqrt(d) / 2 * a;
+            //    Console.WriteLine($"{x1:F2}\n{x2:F2}");
+            //}
+            //if (d == 0)
+            //{
+            //    double x1 = -b / 2 * a;
+            //    Console.WriteLine($"{x1:F2}");
+            //}
+            //if (d<0)
+            //{
+            //    Console.WriteLine($"Нет корней");
+            //}
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
